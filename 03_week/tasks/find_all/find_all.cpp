7e06b1bcf4e63b03ada632254f9cdcd57478857d
lVictorl <milov.2013@bk.ru>
2025-12-16 13:24:36 +0500

    Add solution in find_all.cpp

--- 03_week/tasks/find_all/find_all.cpp
+++ 03_week/tasks/find_all/find_all.cpp
@@ -1,6 +1,25 @@
-#include <stdexcept>
+#include <cstddef>
+#include <vector>
 
+std::vector<size_t> FindAll(const std::vector<int>& vec, bool (*predicate)(int)) {
+    if (!predicate) {
+        return {};
+    }
 
-/* return_type */ FindAll(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+    std::vector<size_t> result;
+
+    // Можно сразу зарезервировать память, но не обязательно
+    // result.reserve(vec.size() / 2); // Для IsEven
+
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (predicate(vec[i])) {
+            result.push_back(i);
+        }
+    }
+
+    // Для выполнения теста CapacityOptimization
+    // shrink_to_fit обеспечит capacity == size
+    result.shrink_to_fit();
+
+    return result;
 }
