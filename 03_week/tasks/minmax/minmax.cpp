d24ba5254b0d816a1aced4ad0c5ce9aa66834b8c
lVictorl <milov.2013@bk.ru>
2025-12-16 13:24:45 +0500

    Add solution in minmax.cpp

--- 03_week/tasks/minmax/minmax.cpp
+++ 03_week/tasks/minmax/minmax.cpp
@@ -1,6 +1,29 @@
-#include <stdexcept>
+#include <utility>
+#include <vector>
 
+std::pair<std::vector<int>::const_iterator, std::vector<int>::const_iterator> MinMax(
+    const std::vector<int>& vec) {
+    // Обработка пустого вектора
+    if (vec.empty()) {
+        return {vec.cend(), vec.cend()};
+    }
 
-/* return_type */ MinMax(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+    // Начинаем с первого элемента
+    size_t min_idx = 0;
+    size_t max_idx = 0;
+
+    // Проходим по всем элементам
+    for (size_t i = 1; i < vec.size(); ++i) {
+        // Обновляем минимум при нахождении меньшего элемента
+        if (vec[i] < vec[min_idx]) {
+            min_idx = i;
+        }
+        // Обновляем максимум при нахождении большего или равного элемента
+        else if (vec[i] >= vec[max_idx]) {
+            max_idx = i;
+        }
+    }
+
+    // Возвращаем итераторы на найденные позиции
+    return {vec.begin() + min_idx, vec.begin() + max_idx};
 }
